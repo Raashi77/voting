7dbd6b222a39bc36cab13459e65b3db74a220950
raashi77 <rashikhatri0013@gmail.com>
2021-02-16 08:08:59 +0530

    dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,13 +2,100 @@
 require_once 'header.php';
 require_once 'navbar.php';
 require_once 'left-navbar.php';
- 
-   
+
+$sql="SELECT count(id) as count from users";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $users=$row['count']; 
+    }
+
+} 
+//fetching blocked comapnies
+$sql="SELECT count(id) as count from contest_users";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $c_users=$row['count']; 
+    }
+
+} 
+
+$sql="SELECT count(id) as count from contest";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $contest=$row['count']; 
+    }
+}
+
+$sql="SELECT count(id) as count from voters";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $voters=$row['count']; 
+    }
+}
+$sql="SELECT count(id) as count from from contest where (end_date < '$date') or (end_date = '$date' and end_time < '$time')";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $c_contest=$row['count']; 
+    }
+}
+$sql="SELECT count(id) as count from contest where (start_date = '$date' and start_time > '$time') or (start_date > '$date')";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        $row=$result->fetch_assoc(); 
+        $u_contest=$row['count']; 
+    }
+}
+
+$sql="SELECT * from contest where (start_date = '$date' and start_time <= '$time') or (start_date < '$date' and end_date > '$date') or (end_date = '$date' and end_time >= $time') limit 8";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        while($row=$result->fetch_assoc())
+        {
+            $cdetails[]=$row; 
+        }
+        
+    }
+}
+
+$sql="select c.name as cname, cu.votes, u.name from contest c, contest_users cu, users u, winner w where w.u_id=u.id and w.u_id=cu.u_id and w.c_id=c.id order by w.id desc limit 5";
+if($result=$conn->query($sql))
+{
+    if($result->num_rows>0)
+    {
+        while($row=$result->fetch_assoc())
+        {
+            $winners[]=$row; 
+        }
+        
+    }
+}
+
+
 
 
 
 ?>
 <!-- Content Wrapper. Contains page content -->
+
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -24,7 +111,180 @@ require_once 'left-navbar.php';
     <!-- overview section -->
 
     <section class="content">
-      
+        <div class="row">
+            <div class="col-md-6">
+                <a href="contest?token=1" style="background-color: white;">
+                    <div class="info-box mb-3 bg-green">
+                        <span class="info-box-icon"><i class="fa fa-tasks"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Contests</span>
+                            <span class="info-box-number"><?=$contest?></span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="#" style="background-color: white;">
+                    <div class="info-box mb-3 bg-red">
+                        <span class="info-box-icon"><i class="fa fa-user-plus"></i></span>
+                        <div class="info-box-content" style="color: white;">
+                            <span class="info-box-text">Voters</span>
+                            <span class="info-box-number"><?=$voters?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <a href="users?token=1" style="background-color: white;">
+                    <div class="info-box mb-3 bg-blue">
+                        <span class="info-box-icon"><i class="fa fa-building"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Users</span>
+                            <span class="info-box-number"><?=$users?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="contestusers?token=1" style="background-color: white;">
+                    <div class="info-box mb-3 bg-yellow">
+                        <span class="info-box-icon"><i class="fa fa-building"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Contest Users</span>
+                            <span class="info-box-number"><?=$c_users?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                </a>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header border-transparent" style="background-color: #343a40;">
+                        <h3 class="card-title" style="color: white;">Ongoing Contests</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table m-0" style="border-spacing: 2px;  font-size: 16px;">
+                                <thead style="font-weight: 800; background-color: #6c757d; color: white;">
+                                    <tr>
+                                        <th style="text-align: center;">S.no.</th>
+                                        <th style="text-align: center;">Name</th>
+                                        <th style="text-align: center;">Start Date</th>
+                                        <th style="text-align: center;">Start Time</th>
+                                        <th style="text-align: center;">End Date</th>
+                                        <th style="text-align: center;">End Time</th>
+                                        <th style="text-align: center;">Prize</th>
+                                    </tr>
+                                </thead>
+                                <tbody style="text-align: center;">
+                                    <?php
+                                        if(isset($cdetails))
+                                        {
+                                            $i=1;
+                                            foreach($cdetails as $data)
+                                            {
+                                    ?>
+                                                <tr>
+                                                    <td style="padding: 12px; color: #17a2b8;"><?=$i?></td>
+                                                    <td style="padding: 12px;"><?=$data['name']?></td>
+                                                    <td style="padding: 12px;"><?=$data['start_date']?></td>
+                                                    <td style="padding: 12px;"><?=$data['start_time']?></td>
+                                                    <td style="padding: 12px;"><?=$data['end_date']?></td>
+                                                    <td style="padding: 12px;"><?=$data['end_time']?></td>
+                                                    <td style="padding: 12px;"><?=$data['prize']?></td>
+                                                </tr>
+                                    <?php
+                                                $i++;
+                                            }
+                                        }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer clearfix">
+                            <a href="contest?token=1" class="btn btn-sm btn-info float-right">View All Contests</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 20px;">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header" style="background-color: black; color:white;">
+                            <h3 class="card-title">Winners</h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Contest</th>
+                                <th>Winner</th>
+                                <th style="width: 40px">Votes</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                    if(isset($winners))
+                                    {
+                                        $i=1;
+                                        foreach($winners as $data)
+                                        {
+                                ?>
+                                                            
+                                            <tr>
+                                            <td><?=$i?></td>
+                                            <td><?=$data['cname']?></td>
+                                            <td>
+                                                <?=$data['name']?>
+                                            </td>
+                                            <td><?=$data['votes']?></td>
+                                            </tr>
+                                <?php
+                                        $i++;    
+                                        }
+                                    }
+                                ?>
+                                
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                <a href="users?token=1" style="background-color: white;">
+                    <div class="info-box mb-3 bg-blue">
+                        <span class="info-box-icon"><i class="fa fa-building"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Upcoming Contests</span>
+                            <span class="info-box-number"><?=$u_contest?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-5">
+                <a href="contestusers?token=1" style="background-color: white;">
+                    <div class="info-box mb-3 bg-yellow">
+                        <span class="info-box-icon"><i class="fa fa-building"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Completed Contests</span>
+                            <span class="info-box-number"><?=$c_contest?></span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                </a>
+            </div>
+        </div>
     </section>
 </div>
 
