15a6e12035077d38d1d5a81688574ef494f75a65
vansh10patpatia <vansh10patpatia@gmail.com>
2021-05-15 23:41:04 +0530

    payment

--- allsongs.php
+++ allsongs.php
@@ -212,6 +212,7 @@ $sql="select * from  songs  ";
         onApprove: function(data, actions) {
           return actions.order.capture().then(function(details) {
             alert('Transaction completed by ' + details.payer.name.given_name);
+            
           });
         }
       }).render('#'+container); // Display payment options on your web page
