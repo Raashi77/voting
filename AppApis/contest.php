baa01ea865c6c2ec1a5f44483aee4f7d44b1fc99
Vansh Patpatia <vanshpatpatia@Vanshs-MacBook-Air.local>
2021-12-04 19:01:32 +0530

    changes done and leaderboard added

--- AppApis/contest.php
+++ AppApis/contest.php
@@ -24,8 +24,71 @@
         echo json_encode($response);
     }   
 
+    if(isset($_POST['fetchWinner']))
+    {
+        $response = [];
+        $contestId = $conn->real_escape_string($_POST['contestId']);
+        $sql = "SELECT u.name,u.profilePic,cu.votes,c.winner from contest c, users u , contest_users cu where c.winner = u.id and c.id='$contestId' and cu.u_id = u.id and cu.c_id = c.id";
+        if($result = $conn->query($sql))
+        {
+            if($result->num_rows > 0)
+            {
+                $response['msg']="success";
+                $response['winner'] = $result->fetch_assoc();
+                $winner = $response['winner']['winner'];
+                $sql3 = "SELECT count(id) as videos from contest_users where u_id='$winner' and c_id = '$contestId'";
+                if($data = $conn->query($sql3))
+                {
+                    if($data->num_rows > 0)
+                    {
+                        // print_r($data->fetch_assoc());
+                        $videos = $data->fetch_assoc();
+                        $response['winner']['videos'] = $videos['videos'];
+                        // $response['videos'] = $sql3;
+                    }
+                    else
+                    {
+                        $response['videos'] = $sql3;
+                    }
+                }
+                else
+                {
+                    $response['error3'] = $conn->error;
+                }
+                $sql2 = "SELECT u.name,u.profilePic,cu.votes from contest_users cu , users u where cu.u_id = u.id and cu.c_id = '$contestId'";
+                if($res = $conn->query($sql2))
+                {
+                    if($res->num_rows > 0)
+                    {
+                        while($row =$res->fetch_assoc())
+                        {
+                            $response['contestants'][] = $row ;
+                        }
+                    }
+                    else
+                    {
+                        $response['contestants'] = "notFound";
+                    }
+                }
+            }   
+            else
+            {
+                $response['msg'] = "noWinner";
+            }
+        }
+        else
+        {
+            $response['msg'] = "error";
+            $response['error'] = $conn->error;
+            $response['query'] = $sql;
+        }
+        echo json_encode($response);
+    }
+
     if(isset($_POST['fetchSingleContest']))
     {
+        $response = [];
+        $date = date("Y-m-d");
         $contestId = $conn->real_escape_string($_POST['contestId']);
         $sql = "SELECT c.*,i.header_image from contest c, index_changes i where c.id  = '$contestId' and c.id=i.c_id";
         if($result = $conn->query($sql))
@@ -34,6 +97,13 @@
             {
                 $response['msg'] = "success";
                 $response['contest'] = $result->fetch_assoc();
+                if($response['contest']['end_date'] < $date)
+                {
+                    $response['contest']['type'] = 'ended' ;
+                }
+                else{
+                    $response['contest']['type'] = 'notEnded';
+                }
                 $sql2 = "SELECT cu.description,cu.votes,cu.status,u.name,u.email,u.profilePic,u.id,v.video,v.time_stamp as videoTime from contest_users cu,users u,videos v WHERE cu.c_id = '$contestId' and v.c_id = cu.c_id and cu.u_id = u.id and v.u_id = u.id";
                 if($res = $conn->query($sql2))
                 {
